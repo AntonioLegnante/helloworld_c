3504b5e3b10ba0a81fea1ad1b5c56527f3f0d105
AntonioLegnante <Antoniolegnante12@gmail.com>
2018-12-24 14:04:01 +0100

    Rappresentazione dei rettangoli

---
+++ Figure.c
@@ -0,0 +1,13 @@
+//Stampa a video di 3 rettangoli
+#include <stdio.h>
+int main () {
+printf("********    ****\n"
+       "*      *    *  *\n"
+       "*      *    *  *\n"
+       "*      *    *  *   *****\n"
+       "*      *    *  *   *   *\n"
+       "*      *    *  *   *   *\n"
+       "********    ****   *****");
+return 0;
+}
+       
