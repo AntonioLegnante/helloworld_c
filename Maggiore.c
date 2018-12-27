0500c9a51d28010a9154982b1a73571cae129fa1
AntonioLegnante <AntonioLegnante12@gmail.com>
2018-12-27 14:18:04 +0100

    Maggiore tra 2 valori

--- Maggiore.c
+++ Maggiore.c
@@ -1,8 +1,7 @@
 //Maggiore tra 2 variabili usando un solo costrutto if a una via
 #include <stdio.h>
 int main () {
-int a,b;
-int massimo=0;
+int a,b,massimo=0;
 printf("inserisci un valore\n");
 scanf("%d",&a);
 printf("inserisci un valore\n");
